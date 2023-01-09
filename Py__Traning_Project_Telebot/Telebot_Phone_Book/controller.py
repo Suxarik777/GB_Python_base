dbba4271d766c9a3b459d98a8be41cd08058c92b
Suxarik777 <gultcev@mail.ru>
2023-01-10 02:45:17 +0300

    Пересмотр логики работы, внедрение кнопок beta3

--- Py__Traning_Project_Telebot/Telebot_Phone_Book/controller.py
+++ Py__Traning_Project_Telebot/Telebot_Phone_Book/controller.py
@@ -1,9 +1,11 @@
 import csv
-from outputs import print_menu, menu_search, show_all, show_by_index, show_by_filter
+from outputs import menu_search, show_all, show_by_index, show_by_filter
 from edits import edit
 from inputs import input_menu_item, input_row, input_index, input_string
 from exports import export_file, export_csv, export_xml, export_html
 from func_for_work_bot import read_file, recording_file
+import emoji
+from str_msg_bot import print_menu, hi_bot
 from input_from_data import recording_str_keyboard
 from telebot import TeleBot, types
 from Token_id import TOKEN_ID
@@ -15,35 +17,39 @@ bot = TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_program(msg: types.Message):
-    # markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2) # растяни по ширине экрана и добавь две кнопк
-    # button_menu = types.KeyboardButton('Меню')
-    # markup.add(button_menu)
-    # mess = f'Привет, <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
-    # bot.send_message(message.chat.id, mess, parse_mode='html')
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2) # растяни по ширине экрана и добавь две кнопк
+    button_menu = types.KeyboardButton(emoji.emojize(':scroll: Меню'))
+    markup.add(button_menu)
 
-    bot.send_message(chat_id=msg.from_user.id, text=f'/start и /help - Начало работы с ботом, вывод списка комманд')
-    bot.send_message(chat_id=msg.from_user.id, text=f'/menu - Начать работу с телефонным справочником')
+    mess = f'''Привет, <b>{msg.from_user.first_name} {msg.from_user.last_name}</b>
+Я создан студентом GeekBrains и я телефонная книжка
+ЖМИ кнопку "Меню"'''
+    bot.send_message(chat_id=msg.chat.id, text=mess, parse_mode='html', reply_markup=markup)
+
+    # bot.send_message(chat_id=msg.from_user.id, text=f'/start и /help - Начало работы с ботом, вывод списка комманд')
+    # bot.send_message(chat_id=msg.from_user.id, text=f'/menu - Начать работу с телефонным справочником')
 
 
 
-@bot.message_handler(commands=['menu'])
+@bot.message_handler(content_types=['text'])
 def menu(msg: types.Message):
-    bot.send_message(chat_id=msg.chat.id, text=f'{print_menu}')
-    bot.register_next_step_handler(msg, get_menu_input)
+    if(msg.text == emoji.emojize(':scroll: Меню')):
+        bot.send_message(chat_id=msg.chat.id, text=f'{print_menu}')
+        bot.register_next_step_handler(msg, get_menu)
 
 
 # 1 menu - ввод данных в телефонную книгу
-def get_menu_input(msg: types.Message):
+def get_menu(msg: types.Message):
     # bot.send_message(chat_id=msg.chat.id, text=f'Я нахожусь в get_user, ваш выбор{msg.text}')
     # menu_item = input_menu_item()
     menu_item = msg.text # получаем то что ввел пользователь
     if menu_item == '1':
         bot.send_message(chat_id=msg.from_user.id, text=f'1 - ввод с клавиатуры \n2 - ввод из файла')
-        bot.register_next_step_handler(msg, get_menu_input_procesing)
+        bot.register_next_step_handler(msg, get_menu_input)
 
 
 
-def get_menu_input_procesing(msg: types.Message):
+def get_menu_input(msg: types.Message):
     menu_item = msg.text
     if menu_item == '1':
         bot.send_message(chat_id=msg.from_user.id, text=f'Введите данные в формате \nИмя Фамилия Телефон Комментарий')
--- Py__Traning_Project_Telebot/Telebot_Phone_Book/input_from_data.py
+++ Py__Traning_Project_Telebot/Telebot_Phone_Book/input_from_data.py
@@ -1,5 +1,10 @@
 from telebot import TeleBot, types
 from func_for_work_bot import read_file, recording_file
+from Token_id import TOKEN_ID
+
+TOKEN = TOKEN_ID
+bot = TeleBot(TOKEN)
+
 
 
 def recording_str_keyboard(msg: types.Message):
@@ -8,3 +13,11 @@ def recording_str_keyboard(msg: types.Message):
     array = read_file()
     array.append(data_list)
     recording_file(array)
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1) # растяни по ширине экрана и добавь две кнопк
+    button_menu = types.KeyboardButton('/menu')
+    markup.add(button_menu)
+    # mess = f'Привет, <b>{message.from_user.first_name} {message.from_user.last_name}</b>'
+    # bot.send_message(message.chat.id, mess, parse_mode='html')
+    text_message = f'Новый контакт\n{text}\n<b>записан</b>'
+    bot.send_message(chat_id=msg.from_user.id, text=text_message, parse_mode='html', reply_markup=markup)
